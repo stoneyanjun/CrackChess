16db144c0f8688ed83c4634a86e8e054f55121b8
stone <stoneyanjun@gmail.com>
2025-11-11 17:25:10 +0800

    Add OpenCV

---
+++ CrackChess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrackChess
+//
+//  Created by stone on 2025/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- CrackChess/CrackChessApp.swift
+++ CrackChess/CrackChessApp.swift
@@ -2,45 +2,16 @@
 //  CrackChessApp.swift
 //  CrackChess
 //
-//  Created by stone on 2025/11/8.
+//  Created by stone on 2025/11/11.
 //
 
-/*
 import SwiftUI
-import ComposableArchitecture
 
 @main
 struct CrackChessApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView(
-                store: Store(
-                    initialState: HomeFeature.State(),
-                    reducer: { HomeFeature() }
-                )
-            )
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .ignoresSafeArea()
+            ContentView()
         }
-        .windowStyle(.hiddenTitleBar)
-    }
-}
-*/
-import SwiftUI
-import ComposableArchitecture
-
-@main
-struct CrackChessApp: App {
-    var body: some Scene {
-        WindowGroup("CrackChess") {
-            HomeView(
-                store: Store(
-                    initialState: HomeState(),
-                    reducer: { HomeFeature() }
-                )
-            )
-            .frame(minWidth: 720, minHeight: 520)
-        }
-        .windowStyle(.automatic)
     }
 }
